26c3f9a8f424ad93cc9149fe1391df2933af8641
alex <alexander7663@gmail.com>
2026-06-30 16:31:16 +0900

    alex7663/feature/turn right3

--- src/example/example/self_driving/self_driving.py
+++ src/example/example/self_driving/self_driving.py
@@ -526,11 +526,9 @@ class SelfDrivingNode(Node):
                     # TODO 01 : start_turn 탈출 조건 else 밖으로 이동 (차선 없어도 탈출 가능)
                 if self.start_turn:
                     turn_elapsed = time.time() - self.start_turn_time_stamp
-                    # TODO : 최소 1초는 회전 유지 (시작 직후 박스5 깜빡임으로 바로 탈출 방지)
-                    if turn_elapsed > 1.0 and len(center_x) >= 5 and center_x[4] != -1:
-                        self.count_turn_exit += (
-                            1  # TODO : 박스5 다시 보이면 회전 끝난 것으로 판단
-                        )
+                    # TODO : 최소 1초는 회전 유지 (시작 직후 깜빡임으로 바로 탈출 방지)
+                    if turn_elapsed > 1.0 and len(center_x) >= 4 and center_x[3] != -1:
+                        self.count_turn_exit += 1  # TODO : 박스5->박스4 기준 변경 - 박스5는 끝까지 안 돌아오는 경우가 많아서 너무 늦게 탈출/영영 탈출 못함
                         if self.count_turn_exit > 5:
                             self.start_turn = False
                             self.count_turn = 0
